338c867def431a206b2fd88c393900859158dec6
Soner Sayakci <s.sayakci@shopware.com>
2023-01-04 19:32:42 +0100

    initial commit

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "Packages for Blackfire";
+
+  inputs.flake-compat = {
+    url = "github:edolstra/flake-compat";
+    flake = false;
+  };
+
+  outputs = { self, nixpkgs, flake-compat }:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    in
+    {
+      formatter = forAllSystems (
+        system:
+        nixpkgs.legacyPackages.${system}.nixpkgs-fmt
+      );
+
+      packages = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          blackfire = pkgs.callPackage ./packages/blackfire { };
+        }
+      );
+    };
+}
